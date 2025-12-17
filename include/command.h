29754a221d7190f7eccdbb54f7ecb5bb73632819
Noah Wager <noahwager@gmail.com>
2025-12-16 23:33:46 -0800

    Add python script to send boardctl commands
    Initial python script to send serial commands to the board controller.
    Requires installing requirements.txt and parsing the command.h header
    for command values using special tags.
    
    Signed-off-by: Noah Wager <noahwager@gmail.com>

--- include/command.h
+++ include/command.h
@@ -4,10 +4,12 @@
 #include <stdint.h>
 
 // Command code definitions
+/* __STARTDEF__ command_code */
 #define CMD_RESET      0
 #define CMD_RECOVERY   1
 #define CMD_POWER      2
-#define CMD_START      0xFF
+#define CMD_START      255
+/* __ENDDEF__ command_code */
 
 typedef uint8_t command_code_t;
 
